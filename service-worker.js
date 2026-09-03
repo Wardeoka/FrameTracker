5358b7e88cfd5667d3c8cae741ec5b95e28d09b0
Wardeoka <wardeoka@yahoo.com>
2026-09-03 11:59:35 +0100

    Add pool hall background and improve audio handling

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = "frame-tracker-v2";
+const CACHE_NAME = "frame-tracker-v4";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./manifest.json",
+    "./icons/apple-touch-icon.png",
+    "./icons/icon-192.png",
     "./icons/icon-512.png",
     "./audio/score.mp3"
+    "./images/background.png",
 ];
 
 self.addEventListener("install", function(event) {
